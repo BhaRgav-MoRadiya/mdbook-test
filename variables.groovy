aca6a6920b9e4d7f13cd7c6de0178de66be7b1d1
BhaRgav-MoRadiya <30456706+BhaRgav-MoRadiya@users.noreply.github.com>
2018-12-20 10:53:23 +0530

    adds return

--- variables.groovy
+++ variables.groovy
@@ -4,4 +4,4 @@ properties["dockerize"] = true
 properties["appName"] = "autoopt-3/staging/crawler/mnet-sample"
 properties["marathonInstances"] = 1
 properties["tag"] = "latest"
-
+return properties

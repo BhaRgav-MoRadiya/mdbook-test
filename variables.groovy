1adbb10074a420d388c3409f1f03be818891f6cf
BhaRgav-MoRadiya <30456706+BhaRgav-MoRadiya@users.noreply.github.com>
2018-12-20 09:58:26 +0530

    Create groovy vars

---
+++ variables.groovy
@@ -0,0 +1,7 @@
+
+def properties = [:]
+properties["dockerize"] = true
+properties["appName"] = "autoopt-3/staging/crawler/mnet-sample"
+properties["marathonInstances"] = 1
+properties["tag"] = "latest"
+
